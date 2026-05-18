de9f759bfc01c484b26c39afd46c3d12c7a145b5
alejandrop-dev <alepersonalgrowth@gmail.com>
2026-05-17 22:23:35 -0500

    Add files via upload

---
+++ Introduccion_1/TiposDeDatosNoPrimitivos/src/Curso_Java6/TiposDeDatosNoPrimitivos.java
@@ -0,0 +1,20 @@
+package Curso_Java6;
+
+public class TiposDeDatosNoPrimitivos {
+
+    
+    public static void main(String[] args) {
+        //Variables no primitivas 
+        System.out.println("***************Variables no primitivas***************\n");
+        
+        //Variable com Integer
+        Integer numero = null;  //Esta variable es de tipo entero no primitiva y acepta el valor de null
+        System.out.println("Variable entera con Integer: " + numero);
+        
+        //Variable com String "Cadenas"
+        String caracteres = "Hola que tal"; //Esta variable es de tipo cadena 
+        System.out.println("Variable de cadenas de caracteres: " + caracteres );
+  
+    }
+    
+}
